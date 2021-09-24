79ae0b95415b4e6e13f75fadebc6c73df8ca4249
wanglin2 <1013335014@qq.com>
2021-09-24 14:03:01 +0800

    修复monaco editor去除内置语言的问题

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 module.exports = {
     publicPath: '/code-run-online/',
     lintOnSave: false,
-    transpileDependencies: ['monaco-editor'],
     configureWebpack: {
         resolve: {
             alias: {
