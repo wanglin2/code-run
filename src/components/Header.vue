d72f49a2a7fcbe5ede35638e76b90d84cfa3d006
wanglin <1013335014@qq.com>
2022-08-27 15:06:40 +0800

    optimization:提取分享组件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -191,20 +191,7 @@
       </div>
     </el-drawer>
     <!-- 分享弹窗 -->
-    <el-dialog 
-      :title="copyDialogTitle" 
-      v-model="shareDialogVisible" 
-      :width="600"
-    >
-      <el-input 
-        v-model="shareUrl" 
-        type="textarea" 
-        autosize 
-        readonly 
-        ref="shareUrlInput"
-      ></el-input>
-      <p class="tip">{{copyDialogTip}}</p>
-    </el-dialog>
+    <Share ref="ShareComp" :isEdit="isEdit"></Share>
   </div>
 </template>
 
@@ -238,7 +225,7 @@ import {
 import { request } from "@/utils/octokit";
 import dayjs from "dayjs";
 import { useRouter, useRoute } from "vue-router";
-import { createShareUrl as createShareUrlUtil, createEmbedUrl as createEmbedUrlUtil } from '@/utils';
+import Share from './Share.vue';
 
 // hooks定义部分
 
@@ -615,48 +602,17 @@ const useDrawer = ({ router, route, githubToken, login }) => {
 };
 
 // 分享
-const shareUrlInput = ref(null);
-const useShare = ({ route, isEdit }) => {
-  const copyDialogTitle = ref('');
-  const copyDialogTip = ref('');
-  const shareDialogVisible = ref(false);
-  const shareUrl = ref("");
-  const showUrl = (url) => {
-    if (!isEdit.value) {
-      return;
+const ShareComp = ref(null);
+const useShare = () => {
+  const methods = {}
+  ;['createShareUrl', 'createEmbedUrl', 'createEmbedCode'].forEach((method) => {
+    methods[method] = (...args) => {
+      ShareComp.value[method](...args);
     }
-    shareUrl.value = url;
-    shareDialogVisible.value = true;
-    nextTick(() => {
-      shareUrlInput.value.select();
-    });
-  }
-  const createShareUrl = () => {
-    copyDialogTitle.value = '分享';
-    copyDialogTip.value = '复制url进行分享吧~'
-    showUrl(createShareUrlUtil(route.params.id));
-  };
-  const createEmbedUrl = () => {
-    copyDialogTitle.value = '嵌入';
-    copyDialogTip.value = '复制url嵌入到你页面的iframe里吧~';
-    showUrl(createEmbedUrlUtil(route.params.id));
-  }
-  const createEmbedCode = () => {
-    copyDialogTitle.value = '嵌入';
-    copyDialogTip.value = '复制代码插入到你页面里吧~';
-    let code = `<iframe height="500" style="width: 100%;" scrolling="no" src="${createEmbedUrlUtil(route.params.id)}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe>`;
-    showUrl(code);
-  }
-  return {
-    copyDialogTitle,
-    copyDialogTip,
-    shareDialogVisible,
-    shareUrl,
-    createShareUrl,
-    createEmbedUrl,
-    createEmbedCode
-  };
-};
+  });
+
+  return methods;
+}
 
 // created部分
 const { proxy, store, layout, router, route } = useInit();
@@ -703,10 +659,11 @@ const {
   gistCurrentChange,
   gistPageNo,
 } = useDrawer({ router, route, githubToken, login });
-const { copyDialogTitle, copyDialogTip, shareDialogVisible, shareUrl, createShareUrl, createEmbedUrl, createEmbedCode } = useShare({
-  route,
-  isEdit,
-});
+const {
+  createShareUrl,
+  createEmbedUrl,
+  createEmbedCode
+} = useShare();
 </script>
 
 <style scoped lang="less">
